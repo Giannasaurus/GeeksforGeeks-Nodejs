d20b65b6a4edcad0ba015c356246ae3213670677
giannasaurus <gianzcantor@gmail.com>
2025-09-04 16:57:49 +0800

    add index.js files to 08-first-application

---
+++ 08-first-application/02-index.js
@@ -0,0 +1,10 @@
+// Creating Web-based Node Application
+var http = require('http');
+
+http.createServer(function (req, res) {
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    res.end('Hello World!');
+
+}).listen(8080);
